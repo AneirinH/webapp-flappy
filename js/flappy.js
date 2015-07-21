2786571d744434bbf7274bf8451ef96705eb89c0
V3513 (Conference ID) <v3513@cam.ac.uk>
2015-07-21 17:02:46 +0100

    Day 2 stuffffff

--- js/flappy.js
+++ js/flappy.js
@@ -1,13 +1,14 @@
 // the Game object used by the phaser.io library
 var stateActions = { preload: preload, create: create, update: update };
 
+
 // Phaser parameters:
 // - game width
 // - game height
 // - renderer (go for Phaser.AUTO)
 // - element where the game will be drawn ('game')
 // - actions on the game state (or null for nothing)
-var game = new Phaser.Game(640, 350, Phaser.AUTO, 'game', stateActions);
+var game;// = new Phaser.Game(640, 350, Phaser.AUTO, 'game', stateActions);
 var score;
 score = 0;
 var labelscore;
@@ -18,6 +19,13 @@ var pipes = [];
  * Loads all resources for the game and gives them names.
  * game.add.sprite(event.x, event.y, "playerImg2");
  */
+jQuery("#greeting-form").on("submit", function(event_details) {
+    var greeting = "Hello ";
+    var name = jQuery("#fullName").val();
+    jQuery("#greeting-form").hide();
+    jQuery("#greeting").append("<p>" + greeting_message + "</p>");
+    event_details.preventDefault();
+});
 function preload() {
     game.load.image("playerImg", "../assets/flappy_frog.png");
     game.load.image("backgroundImg","../assets/f.jpg");
@@ -31,6 +39,10 @@ function clickHandler(event) {
 }
 
 var lastGap = 2;
+
+
+game = new Phaser.Game(640, 350, Phaser.AUTO, 'game', stateActions);
+
 
 function generatePipe(x) {
         x += 750;
@@ -68,13 +80,30 @@ function generatePipe(x) {
         }
 }
 
+
+
+
 function create() {
+
     game.add.image(0,0, "backgroundImg");
+    player = game.add.sprite(100,300,"playerImg2");
+    player.scale.setTo(0.01, 0.01);
+    game.input
+        .keyboard.addKey(Phaser.Keyboard.SPACEBAR)
+        .onDown.add(start);
+
+}
+
+
+function start() {
+    score = 0;
+
+
     for (var count=2;count < 100; count+=3){
         generatePipe(count * 50);
     }
     game.add.text(100, 50, "Welcome to our bouncy ball game",
-    {font: "30px Hobo Std Medium", fill: "#FFFFFF"});
+        {font: "30px Hobo Std Medium", fill: "#FFFFFF"});
 
 
     game.add.sprite(10, 10, "playerImg");
@@ -84,12 +113,17 @@ function create() {
     game.input
         .onDown
         .add(clickHandler);
+
+
+    game.input
+        .keyboard.addKey(Phaser.Keyboard.SPACEBAR)
+        .onDown.removeAll();
     game.input
         .keyboard.addKey(Phaser.Keyboard.SPACEBAR)
         .onDown.add(spaceHandler);
     labelscore = game.add.text(270,20, "Score = 0",
-    {font: "20px Hobo Std Medium", fill: "#FFFFFF"});
-    player = game.add.sprite(100,200,"playerImg2");
+        {font: "20px Hobo Std Medium", fill: "#FFFFFF"});
+
 
 
     game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -100,16 +134,15 @@ function create() {
     player.scale.setTo(0.10, 0.10);
     player.x = 150;
     player.y = 200;
-    player.body.velocity.x = 50;
+    //player.body.velocity.x = 50;
     player.body.velocity.y = -50;
     player.body.gravity.y = 400;
     game.input.keyboard
         .addKey(Phaser.Keyboard.SPACEBAR)
         .onDown.add(playerJump);
 
-
-
 }
+
 function playerJump() {
     player.body.velocity.y = -200;
 
@@ -154,7 +187,14 @@ function changescore(){
 /*
  * This function updates the scene. It is called for every new frame.
  */
+
 function update() {
+    if(player.y > 350){
+        gameOver()
+    }
+    if(player.y< 0){
+        gameOver()
+    }
     for (var index = 0; index < pipes.length; index++) {
         game.physics.arcade
             .overlap(player,
@@ -166,6 +206,17 @@ function update() {
 
 }
 function gameOver(){
-    location.reload();
-
+    game.destroy();
+    alert("You score: " + score + "\nPress OK to retry...");
+    $("#score").val(score.toString());
+    $("#greeting").show();
 }
+
+$.get("/score", function(scores){
+    for (var i = 0; i < scores.length; i++) {
+        $("#scoreBoard").append(
+        "<li>" +
+        scores[i].name + ": " + scores[i].score +
+        "</li>");
+    }
+});
